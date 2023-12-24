d01b32310c2521a7c52dc6cf9c3328c4981b8bd0
sravanthi1606 <151130251+sravanthi1606@users.noreply.github.com>
2023-12-24 20:01:01 +0530

    updated task

---
+++ task.js
@@ -0,0 +1,33 @@
+var num1=12;
+console.log(num1);
+console.log(typeof num1);
+
+var num2=12345678912345678n;
+console.log(num2);
+console.log(typeof num2);
+
+var str="Hello";
+console.log(str);
+console.log(typeof str);
+
+var isGreater=true;
+console.log(isGreater);
+console.log(typeof isGreater);
+
+var num3=null;
+console.log(num3);
+console.log(typeof num3);
+
+var num4=undefined;
+console.log(num4);
+console.log(typeof num4);
+
+var emp={
+    id:12234,
+    name:"sravanthi",
+    isPermanent:true,
+    phoneNumber:null,
+}
+console.log(emp);
+console.log(typeof emp);
+
