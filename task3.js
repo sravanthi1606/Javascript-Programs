1dbc4a17a79009b832d66cdbfb504b3a6b9ce2aa
sravanthi1606 <151130251+sravanthi1606@users.noreply.github.com>
2023-12-26 20:16:50 +0530

    updated task3

---
+++ task3.js
@@ -0,0 +1,11 @@
+var gender = "Female";
+var age = 19;
+if (gender === "Male" && age >= 21) {
+    console.log('Your are eligible for marriage --- boy')
+}
+else if (gender === "Female" && age >= 18) {
+    console.log('Your are  eligible for marriage --- girl')
+}
+else{
+    console.log('Your are not  eligible for marriage')
+}
