b6c2adb96032d3c28ffe73c804ddedb1f5c192a0
sravanthi1606 <151130251+sravanthi1606@users.noreply.github.com>
2023-12-26 20:17:31 +0530

    updated task4

---
+++ task4.js
@@ -0,0 +1,41 @@
+var month = "June";
+switch (month) {
+    case "January":
+        console.log("The Given Month is:", month);
+        break;
+    case "Febrauray":
+        console.log("The Given Month is:", month);
+        break;
+    case "March":
+        console.log("The Given Month is:", month);
+        break;
+    case "April":
+        console.log("The Given Month is:", month);
+        break;
+    case "May":
+        console.log("The Given Month is:", month);
+        break;
+    case "June":
+        console.log("The Given Month is:", month);
+        break;
+    case "July":
+        console.log("The Given Month is:", month);
+        break;
+    case "August":
+        console.log("The Given Month is:", month);
+        break;
+    case "September":
+        console.log("The Given Month is:", month);
+        break;
+    case "October":
+        console.log("The Given Month is:", month);
+        break;
+    case "November":
+        console.log("The Given Month is:", month);
+        break;
+    case "December":
+        console.log("The Given Month is:", month);
+        break;
+    default:
+        console.log("Enter a invalid Month");
+}
